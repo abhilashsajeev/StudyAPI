3a6f52b2d1761da738516f6820cb1aae01f2a351
Abhilash <abhilash@qburst.com>
2015-07-21 17:26:28 +0530

    adding shop cart Api

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# StudyAPI
-Simple Study API
+The OpenShift `nodejs` cartridge documentation can be found at:
+
+http://openshift.github.io/documentation/oo_cartridge_guide.html#nodejs
--- server.js
+++ server.js
@@ -1,89 +1,223 @@
-var express = require('express'),
-	path = require('path'),
-	app = express(),
-	compression = require('compression'),
-	jsonfile = require('jsonfile');
-	require("node-jsx").install();
+#!/bin/env node
+//  OpenShift sample Node application
+var express = require('express');
+var fs      = require('fs');
+var compression = require('compression'),
+        path = require('path'),
+    jsonfile = require('jsonfile');
 
-var shoppingData = __dirname +"/shoppingData.json";
-var jobData = __dirname +"/jobData.json";
-var request = require('superagent');
-	var port = 3000,
-	bodyParser = require('body-parser');
+    var shoppingData = __dirname +"/shoppingData.json";
+    var jobData = __dirname +"/jobData.json";
+	var cartData = __dirname + "/cartData.json";
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+        var  bodyParser = require('body-parser');
 
+/**
+ *  Define the sample application.
+ */
+var SampleApp = function() {
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.set('env', 'production');
-app.use(compression());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Cache-Control", "public, max-age=34560"); 
-  res.setHeader("Expires", new Date(Date.now() + 34560).toUTCString());
-  next();
-});
+    //  Scope.
+    var self = this;
 
 
-app.get('/api/get_shop_data', function (req, res) {
+    /*  ================================================================  */
+    /*  Helper functions.                                                 */
+    /*  ================================================================  */
+
+    /**
+     *  Set up server IP address and port # using env variables/defaults.
+     */
+    self.setupVariables = function() {
+        //  Set the environment variables we need.
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+        
+       
+
+        if (typeof self.ipaddress === "undefined") {
+            //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+            //  allows us to run/test the app locally.
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+            self.ipaddress = "127.0.0.1";
+        };
+    };
+
+
+    /**
+     *  Populate the cache.
+     */
+    self.populateCache = function() {
+        if (typeof self.zcache === "undefined") {
+            self.zcache = { 'index.html': '' };
+        }
+
+        //  Local cache for static content.
+        self.zcache['index.html'] = fs.readFileSync('./index.html');
+    };
+
+
+    /**
+     *  Retrieve entry (content) from cache.
+     *  @param {string} key  Key identifying content to retrieve from cache.
+     */
+    self.cache_get = function(key) { return self.zcache[key]; };
+
+
+    /**
+     *  terminator === the termination handler
+     *  Terminate server on receipt of the specified signal.
+     *  @param {string} sig  Signal to terminate on.
+     */
+    self.terminator = function(sig){
+        if (typeof sig === "string") {
+           console.log('%s: Received %s - terminating sample app ...',
+                       Date(Date.now()), sig);
+           process.exit(1);
+        }
+        console.log('%s: Node server stopped.', Date(Date.now()) );
+    };
+
+
+    /**
+     *  Setup termination handlers (for exit and a list of signals).
+     */
+    self.setupTerminationHandlers = function(){
+        //  Process on exit and signals.
+        process.on('exit', function() { self.terminator(); });
+
+        // Removed 'SIGPIPE' from the list - bugz 852598.
+        ['SIGHUP', 'SIGINT', 'SIGQUIT', 'SIGILL', 'SIGTRAP', 'SIGABRT',
+         'SIGBUS', 'SIGFPE', 'SIGUSR1', 'SIGSEGV', 'SIGUSR2', 'SIGTERM'
+        ].forEach(function(element, index, array) {
+            process.on(element, function() { self.terminator(element); });
+        });
+    };
+
+
+    /*  ================================================================  */
+    /*  App server functions (main app logic here).                       */
+    /*  ================================================================  */
+
+    /**
+     *  Create the routing table entries + handlers for the application.
+     */
+    self.createRoutes = function() {
+        self.routes = { };
+
+        self.routes['/asciimo'] = function(req, res) {
+            var link = "http://i.imgur.com/kmbjB.png";
+            res.send("<html><body><img src='" + link + "'></body></html>");
+        };
+
+        self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(self.cache_get('index.html') );
+        };
+
+
+    };
+
+
+    /**
+     *  Initialize the server (express) and create the routes and register
+     *  the handlers.
+     */
+    self.initializeServer = function() {
+        self.createRoutes();
+        self.app = express();
+        
+
+        self.app.use(bodyParser.json());
+        self.app.use(bodyParser.urlencoded({
+            extended: true
+        }));
+        self.app.use(compression());
+        self.app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          res.setHeader("Cache-Control", "public, max-age=34560"); 
+          res.setHeader("Expires", new Date(Date.now() + 34560).toUTCString());
+          next();
+        });
+
+        self.app.get('/api/get_shop_data', function (req, res) {
   
-  jsonfile.readFile(shoppingData, function(err, obj) {
-  	res.status(200).jsonp(obj);
-  })
+          jsonfile.readFile(shoppingData, function(err, obj) {
+            res.status(200).jsonp(obj);
+          })
 
-});
-app.get('/api/get_job_data', function (req, res) {
-  
-  jsonfile.readFile(jobData, function(err, obj) {
-  	res.status(200).jsonp(obj);
-  })
+        });
+        self.app.get('/api/get_job_data', function (req, res) {
+          
+          jsonfile.readFile(jobData, function(err, obj) {
+            res.status(200).jsonp(obj);
+          })
 
-});
-app.post('/api/set_stock_data', function (req, res) {
-  
-	console.log(req.body);
-	if(req.body.name ===""||req.body.name ===null||req.body.name === undefined){
-		res.send("501");
-	}else{
-		jsonfile.readFile(file_jiyo, function(err, obj) {
-	  		if(req.body.id in obj.results){
-	  			obj.results[req.body.id].name = req.body.name;
-	  		}else{
-	  			var new_obj = {"id":obj.results.length,"name":req.body.name};		
-		  		obj.results.push(new_obj);
-	  		}
-	  		jsonfile.writeFileSync(file_jiyo, obj, {spaces: 4});
-	  		res.send("200")	;
-  		})
-	}
-	
-});
+        });
+		self.app.get('/api/get_shop_cart',function(req,res){
+			
+			jsonfile.readFile(cartData, function (err,obj) {
+  				res.status(200).jsonp(obj);
+			});
+		});
+		self.app.post('/api/set_shop_cart',function(req,res){
+			
+			var obj = req.body;
+			jsonfile.writeFile(cartData, obj, function (err) {
+				res.status(500);
+			});
+			res.end("success");
+		});
+		self.app.post('/api/checkout_shop_cart',function(req,res){
+			
+			var obj = {};
+			jsonfile.writeFile(cartData, obj, function (err) {
+				res.status(500);
+			});
+			res.end("success");
+		});
+		
 
-app.post('/api/priya/set_person_data', function (req, res) {
-  
-	console.log(req.body);
-	if(req.body.name ===""||req.body.name ===null||req.body.name === undefined){
-		res.send("501");
-	}else{
-		jsonfile.readFile(file_priya, function(err, obj) {
-	  		if(req.body.id in obj.results){
-	  			obj.results[req.body.id].name = req.body.name;
-	  		}else{
-	  			var new_obj = {"id":obj.results.length,"name":req.body.name};		
-		  		obj.results.push(new_obj);
-	  		}
-	  		jsonfile.writeFileSync(file_priya, obj, {spaces: 4});
-	  		res.send("200")	;
-  		})
-	}
-	
-});
+        //  Add handlers for the app (from the routes).
+        for (var r in self.routes) {
+            self.app.get(r, self.routes[r]);
+        }
+    };
 
 
-app.listen(port);
+    /**
+     *  Initializes the sample application.
+     */
+    self.initialize = function() {
+        self.setupVariables();
+        self.populateCache();
+        self.setupTerminationHandlers();
 
-console.log('Server is Up and Running at Port : ' + port);
+        // Create the express server and routes.
+        self.initializeServer();
+    };
+
+
+    /**
+     *  Start the server (starts up the sample application).
+     */
+    self.start = function() {
+        //  Start the app on the specific interface (and port).
+        self.app.listen(self.port, self.ipaddress, function() {
+            console.log('%s: Node server started on %s:%d ...',
+                        Date(Date.now() ), self.ipaddress, self.port);
+        });
+    };
+
+};   /*  Sample Application.  */
+
+
+
+/**
+ *  main():  Main code.
+ */
+var zapp = new SampleApp();
+zapp.initialize();
+zapp.start();
+
